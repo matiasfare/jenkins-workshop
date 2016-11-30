786e36246107ccf2deb820e095494b9bb5b72d77
Rodrigo Villalba <rodris.jvo@gmail.com>
2016-11-30 16:25:07 -0300

    Borramos codigo que no usamos

--- src/main/java/py/com/sodep/SqlApplication.java
+++ src/main/java/py/com/sodep/SqlApplication.java
@@ -19,19 +19,6 @@ public class SqlApplication implements CommandLineRunner {
 	@Autowired
 	private JdbcProperties props;
 	
-	private static void callStoredProcedure(Connection conn) throws SQLException {
-		CallableStatement ocs = conn.prepareCall("{? = call acpks_stmt_gen.fn_stmt_gen(?,?,?,?,?,?)}");
-		ocs.registerOutParameter(1, java.sql.Types.ARRAY);
-		ocs.setString(2, "144000014");
-		ocs.setString(3, "RET");
-		ocs.setString(4, "N");
-		ocs.setString(5, "3");
-		ocs.setNull(6, java.sql.Types.DATE);
-		ocs.setNull(7, java.sql.Types.DATE);
-
-		ocs.executeUpdate();
-	}
-
 	@Override
 	public void run(String... arg0) throws Exception {
 	
